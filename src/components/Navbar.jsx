ede8651421251ec9d6130a955f19866376bac258
himanshu <hmxnsu.blvck@gmail.com>
2023-03-20 01:23:52 +0530

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { setTheme } from '../states'
-import { useSelector, useDispatch } from 'react-redux'
-import NavButton from './NavButton'
-import { navlinks } from '../data'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { setTheme } from "../states";
+import { useSelector, useDispatch } from "react-redux";
+import NavButton from "./NavButton";
+import { navlinks, socials } from "../data";
+import Socials from "./Socials";
 
 function Navbar() {
-  const dispatch = useDispatch()
-  const theme = useSelector((state) => state.theme)
-  return (
-    <div className=' w-full h-[44px] flex flex-row justify-end gap-3 '>
-      {navlinks.map((link) => (
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme);
+  const [isActive, setIsActive] = useState("home");
+  const navigate = useNavigate();
 
-        <NavButton 
-        key={link.id}
+  useEffect(() => {
+    const url = window.location.href;
+    const urlArr = url.split("/");
+    const lastUrl = urlArr[urlArr.length - 1];
+    const final = lastUrl === "" ? "Home" : lastUrl;
+    setIsActive(final);
+  }, [window.location.href]);
+
+  return (
+    <div className=" flex flex-row max-lg:flex-col justify-between gap-3 ">
+      <div className=" flex flex-row justify-end gap-3  ">
+        {socials.map((link) => (
+          <Socials key={link.id} data={link} />
+        ))}
+      </div>
+      <div className=" flex flex-wrap justify-end gap-3 ">
+        {navlinks.map((link) => (
+          <div
+            onClick={() =>
+              setIsActive(link.title.toLocaleLowerCase()) + navigate(link.path)
+            }
+          >
+            <NavButton
+              key={link.id}
+              isActive={isActive}
+              theme={theme}
+              text={link.title}
+              icon={">"}
+              length={link.data.length}
+            />
+          </div>
+        ))}
+        <NavButton
           theme={theme}
-          to={link.path}
-          text={link.title}
-          icon={'>'}
-          length={link.data.length}
-        />
-      ))}
-      <NavButton 
-          theme={theme}
-          to={''}
-          text={ theme.mode !== 'dark' ? <img src="https://cdn-icons-png.flaticon.com/512/5043/5043139.png" width="24" height="26" alt="Moon free icon" title="Moon free icon"></img> : <img src="https://cdn-icons-png.flaticon.com/512/1687/1687737.png" className=' invert
-           ' width="24" height="24" alt="Brightness free icon" title="Brightness free icon"></img>}
+          to={""}
+          text={
+            theme.mode !== "dark" ? (
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/5043/5043139.png"
+                width="28"
+                height="26"
+                alt="Moon free icon"
+                title="Moon free icon"
+              ></img>
+            ) : (
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/1687/1687737.png"
+                className=" invert mx-[2px]
+           "
+                width="24"
+                height="24"
+                alt="Brightness free icon"
+                title="Brightness free icon"
+              ></img>
+            )
+          }
           // icon={'>'}
           onClick={() => {
             dispatch(setTheme());
-          }
-          }
+          }}
         />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
