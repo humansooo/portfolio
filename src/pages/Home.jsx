502ffe353ae2593849a00be1f9587952a9de4e92
himanshu <hmxnsu.blvck@gmail.com>
2023-03-21 16:28:40 +0530

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,34 @@ import { Navbar } from "../components";
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../states";
 import AboutBanner from "../components/AboutBanner";
-import { socials } from "../data";
+import { projects, socials } from "../data";
 import Socials from "../components/Socials";
-
+import { randomColor } from "../utils";
+import Slider from "react-slick";
 
 function Home() {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+
+    // slidesToScroll: 1,
+  };
+
   return (
     <>
-    <title>Himanshu Suthar</title>
+      <title>Himanshu Suthar</title>
 
       {/* <div className=" flex sm:flex-col-reverse flex-col "> */}
-        <div className=" mt-6 ">
-          <AboutBanner />
-        </div>
-        <div className="  flex-row hidden mt-6 max-sm:flex justify-end gap-3  ">
+      <div className=" mt-3 sm:mt-6 ">
+        <AboutBanner />
+      </div>
+      
+      <div className="  flex-row hidden mt-6 max-sm:flex justify-end gap-3  ">
         {socials.map((link) => (
           <Socials key={link.id} data={link} />
         ))}
