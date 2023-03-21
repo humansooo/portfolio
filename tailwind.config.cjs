6870d65a7c59794f8d4fdd51b5bfd98b4bae770d
himanshu <hmxnsu.blvck@gmail.com>
2023-03-21 18:13:52 +0530

    ...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,12 +8,31 @@ module.exports = {
     extend: {
       animation: {
         "spin-slow": "spin 3s linear infinite",
+        "blur-zoom-in": "blur-zoom-in 0.3s ease-out",
       },
       keyframes: {
         "spin-slow": {
           from: { transform: "rotate(0deg)" },
           to: { transform: "rotate(360deg)" },
         },
+        "blur-zoom-in": {
+          "0%": {
+            opacity: 0,
+            // transform: "scale(0.9)",
+            filter: "blur(6px)",
+            // transform: "translateX(40%)",
+          },
+          '60%': {
+            filter: "blur(5px)",
+            // transform: "translateX(10%)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "scale(1)",
+            filter: "blur(0)",
+            transform: "translateX(0)",
+          },
+        }
       }
     },
   },
