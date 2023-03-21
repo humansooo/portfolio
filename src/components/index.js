a5ea4f57ee70daca0cf7b34197d7afd0030482a6
himanshu <hmxnsu.blvck@gmail.com>
2023-03-21 16:28:20 +0530

    ...

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Navbar } from './Navbar';
+export { default as NavButton } from './NavButton';
+export { default as Socials } from './Socials';
--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,10 +1,11 @@
 export const darkTheme = {
     primary: '#181818',
+    oppPrimary: '#eaeaea',
     background: '#000',
     secondary: '#ff9800',
     accent: '#ff9800',
     text: '#ffffff',
-    opText: '#000000',
+    opText: '#222',
     lightText: '#ccc',
     border: '#424242',
     lightBorder: '#616161',
@@ -13,10 +14,11 @@ export const darkTheme = {
 
 export const lightTheme = {
     primary: '#eaeaea',
+    oppPrimary: '#181818',
     background: '#eee',
     secondary: '#ff6f00',
     accent: '#ff6f00',
-    text: '#000000',
+    text: '#333',
     opText: '#fff',
     lightText: '#444',
     border: '#e0e0e0',
