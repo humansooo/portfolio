b5505d19e775bad0d1a25c30bc0e91475b5d3d7e
himanshu <hmxnsu.blvck@gmail.com>
2023-03-22 01:51:46 +0530

    ...

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function ContactPage() {
+  const theme = useSelector((state) => state.theme)
   return (
     <>
       <div style={{
         color: theme.lightText,
-      }} className=" lg:w-56 pl-2 scroll text-[24px] animate-blur-in-0 leading-snug  mb-3 py-2 font-[800] uppercase rounded-full ">
-            projects
+      }} className=" pl-2 scroll text-[24px] animate-blur-in-0 leading-snug  mb-3 py-2 font-[800] uppercase rounded-full ">
+        Connect with me
       </div>
     </>
   )
