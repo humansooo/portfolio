2c3948233d487ea3014084b81cc4ab23be8ab59d
himanshu <hmxnsu.blvck@gmail.com>
2023-03-21 16:28:27 +0530

    ...

--- src/components/AboutBanner.jsx
+++ src/components/AboutBanner.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { aboutMe } from "../data";
 import { useSelector } from "react-redux";
 import { randomColor } from "../utils";
+import NavButton from "./NavButton";
 
 function AboutBanner() {
   const theme = useSelector((state) => state.theme);
@@ -11,7 +12,7 @@ function AboutBanner() {
       style={{ background: randomColor().color, color: randomColor().text }}
       className={` w-full min-h-[300px] flex flex-row max-md:flex-col-reverse  justify-between p-8 rounded-[20px] `}
     >
-      <div>
+      <div className=" flex-1 ">
         <div>
           <h1
             // style={{ color: theme.primary }}
@@ -22,19 +23,30 @@ function AboutBanner() {
         </div>
         <div className=" mt-6  ">
           <h1
-            style={{ color: randomColor().text }} 
-            className=" text-4xl max-md:text-3xl font-[900] "
+            style={{ color: randomColor().text }}
+            className=" text-2xl max-md:text-xl font-[600] "
           >
-            {aboutMe.description}'
+            {aboutMe.description.split(" ").slice(0, 20).join(" ")}'
           </h1>
+        </div>
+        <div className=' w-[250px] flex flex-wrap mt-6 '>
+          <NavButton
+            theme={theme}
+            invert={true}
+            textColor={'#ffffff'}
+            to="/About"
+            text="Read More"
+            icon=">"
+            length={null}
+          />
         </div>
       </div>
 
-      <div className=" flex justify-center items-center max-sm:pb-6 ">
+      {/* <div className=" flex justify-center items-center max-sm:pb-6 ">
         <div className=" sm:h-[200px] sm:w-[200px] h-[100px] w-[100px] rounded-full overflow-clip grayscale ">
           <img src={aboutMe.image} alt="image" className=" " />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-function NavButton({ theme, to, text, icon, onClick, length, isActive }) {
-
+function NavButton({ theme, to, text, icon, onClick, length, isActive, invert, textColor }) {
   return (
-    <div
-      style={{ color: theme.lightText }}
+    <Link
+      to={to}
+      style={{ color: textColor ? textColor : theme.lightText }}
       onClick={onClick}
       className={
         ` gap-2 sm:hover:gap-6 max-sm:text-sm max-sm:gap-1 cursor-pointer text-[16px] max-sm:p-1.5 max-sm:pl-3 duration-500 h-full flex justify-between items-center p-2 pl-4 border-[2px] rounded-full ` +
         `${
           theme.mode === "light" ? "border-[#00000056]" : "border-[#ffffff24]"
         } ` +
-        `${!icon ? " sm:hover:gap-0 pl-[8px] max-sm:pl-[6px] " : "sm:hover:pl-5"}`
+        `${
+          !icon ? " sm:hover:gap-0 pl-[8px] max-sm:pl-[6px] " : "sm:hover:pl-5"
+        }`
+         + ` ${invert && "border-[#ffffff3f]"}`
       }
     >
-      <h1>
-        {text}
-      </h1>
+      <h1>{text}</h1>
       {icon && (
         <div
           className={
@@ -27,13 +28,16 @@ function NavButton({ theme, to, text, icon, onClick, length, isActive }) {
               theme.mode === "light"
                 ? "border-[#9b9b9b] sm:hover:border-[#5f5e5e]"
                 : "border-[#5b5b5b] sm:hover:border-[#c3c3c3]"
-            }` + ` ${length && "px-8 ml-4 border-none bg-[#ff6f00]"}` + ` ${isActive && isActive === text && "border-[#ff6f00]"}`
+            }` +
+            ` ${length && "px-8 ml-4 border-none bg-[#ff6f00]"}` +
+            ` ${isActive && isActive === text && "border-[#ff6f00]"}` +  ` ${invert && "border-[#ffffff3f] sm:hover:border-[#ffffffa9]"}`
+
           }
         >
-          { length ? length : icon}
+          {length ? length : icon}
         </div>
       )}
-    </div>
+    </Link>
   );
 }
 
