12d2e0edf63f5fd897e854b6ded3dcd52a7a78bc
himanshu <hmxnsu.blvck@gmail.com>
2023-03-24 01:50:49 +0530

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,5 @@ export { default as ProjectPage } from './ProjectPage';
 export { default as ContactPage } from './ContactPage';
 export { default as AboutPage } from './AboutPage';
 export { default as NotFound } from './NotFound.jsx';
+export { default as BlogDisplay } from './BlogDisplay.jsx';
+export { default as BlogsPage } from './BlogsPage.jsx';
