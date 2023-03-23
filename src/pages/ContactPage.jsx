3a3ed77b33904e51c068a4f1a9fac628f436ac1d
himanshu <hmxnsu.blvck@gmail.com>
2023-03-23 20:24:34 +0530

    ...

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,10 +4,10 @@ import { useSelector } from 'react-redux'
 function ContactPage() {
   const theme = useSelector((state) => state.theme)
   return (
-    <div className='pt-[7rem] sm:pt-[4rem] h-screen  p-3 sm:p-6 '>
+    <div className='pt-[7rem] md:pt-[5rem] h-screen animate-blur-slide-down-10 p-3 sm:p-6 '>
       <div style={{
         color: theme.lightText,
-      }} className=" pl-2  scroll text-[24px] animate-blur-in-0 leading-snug  mb-3 py-2 font-[800] uppercase rounded-full ">
+      }} className=" pl-2  scroll text-[24px] leading-snug  mb-3 py-2 font-[800] uppercase rounded-full ">
         Connect with me
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Navbar } from "../components";
+import { Navbar, ProjectCard } from "../components";
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../states";
 import AboutBanner from "../components/AboutBanner";
 import { projects, socials } from "../data";
 import Socials from "../components/Socials";
 import { randomColor } from "../utils";
+import { Link } from "react-router-dom";
 
 function Home() {
   const dispatch = useDispatch();
@@ -24,29 +25,27 @@ function Home() {
     <>
       <title>Himanshu Suthar</title>
 
-      <div className=" mt-[7rem] sm:mt-[4rem] p-3 sm:p-6 animate-blur-in ">
+      <div className=" mt-[7rem] lg:mt-[4rem] px-3 sm:px-6 pt-3 sm:pt-6 animate-blur-in ">
         <AboutBanner />
       </div>
 
       <div
         className={
-          " mt-3 sm:mt-6 sm:m-6 m-3 p-3 h-[97vh] border rounded-[24px] animate-blur-in " +
-          `${
-            theme.mode === "light" ? "border-[#00000049]" : "border-[#ffffff49]"
-          }`
+          " mt-3 sm:mt-3 sm:mx-6 mb-6 mx-3 border-0 flex flex-col gap-3 rounded-[24px] animate-blur-in " 
+          +`${theme.mode === "light" ? "border-[#00000049]" : "border-[#ffffff49]"}`
         }
-      >
-        <div
-          className={
-            ` h-full w-full border rounded-[14px] animate-blur-in ` +
-            `${
-              theme.mode === "light"
-                ? "border-[#00000049]"
-                : "border-[#ffffff49]"
-            }`
-          }
-        ></div>
+        >
+        <Link to={'/Projects'} className={" text-xl duration-500 text-center after:container border p-2 px-3 hover:px-6 rounded-full  hover:blur-[1px] font-semibold " + `${theme.mode === "light" ? "border-[#00000049]" : "border-[#ffffff49]"}`
+        }
+        >Projects</Link>
+        <div className="flex flex-wrap max-md:flex-wrap gap-3">
+        {/* projects */}
+          {/* <ProjectCard data={projects[0]} /> */}
+          <ProjectCard data={projects[1]} />
+          <ProjectCard data={projects[2]} />
+          <ProjectCard data={projects[2]} />
       </div>
+          </div>
 
       {/* bottom social links */}
       <div className="  flex-row hidden mt-6 max-sm:flex justify-end gap-3  ">
